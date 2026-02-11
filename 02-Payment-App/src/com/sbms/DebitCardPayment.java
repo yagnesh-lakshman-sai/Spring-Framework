de1e0598e9e7df7e0b1f96708605e85a8dc594f0
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-11 21:37:56 +0530

    add DebitCardPayment class IPayment Interface

---
+++ 02-Payment-App/src/com/sbms/DebitCardPayment.java
@@ -0,0 +1,12 @@
+package com.sbms;
+
+public class DebitCardPayment implements IPayment {
+
+	@Override
+	public boolean processPayment(double billAmt) {
+		// logic
+		System.out.println("Debit Card Payment Processed...");
+		return true;
+	}
+
+}
