8d21e737e1047af1754b69128bfd0d92d9b3bb2e
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-11 21:35:55 +0530

    Add App main to wire PaymentService

---
+++ 02-Payment-App/src/com/sbms/App.java
@@ -0,0 +1,15 @@
+package com.sbms;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		IPayment p1 = new CreditCardPayment();
+
+		// constructor injection
+		PaymentService ps = new PaymentService(p1);
+
+		ps.doPayment(100.00);
+
+	}
+}
