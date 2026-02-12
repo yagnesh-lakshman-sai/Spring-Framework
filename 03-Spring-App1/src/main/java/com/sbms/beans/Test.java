2155755a6f646b1eec8a41d44f82961422ebef5e
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-12 21:14:15 +0530

    Test Main method

---
+++ 03-Spring-App1/src/main/java/com/sbms/beans/Test.java
@@ -0,0 +1,26 @@
+package com.sbms.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("Beans.xml");
+		
+		PaymentService service = 
+				context.getBean(PaymentService.class);
+		
+		service.doPayment(199.00);
+
+	}
+}
+
+
+
+
+
+
+
