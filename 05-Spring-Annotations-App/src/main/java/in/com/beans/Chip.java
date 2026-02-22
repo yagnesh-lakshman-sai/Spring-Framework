ea19c697ea52679112369b76bceff8fa5fa987b9
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-22 20:34:17 +0530

    Add Spring component class Chip in beans package

---
+++ 05-Spring-Annotations-App/src/main/java/in/com/beans/Chip.java
@@ -0,0 +1,12 @@
+package in.com.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Chip {
+	
+	public Chip() {
+		System.out.println("Chip :: Constructor");
+	}
+	
+}
