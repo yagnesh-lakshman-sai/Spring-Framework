89718b206cb8eca34250d4818d765a78dac024c3
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-24 22:52:51 +0530

    Add ReportService with constructor injection

---
+++ 06-Spring-Autowiring-App/src/main/java/in/com/beans/ReportService.java
@@ -0,0 +1,19 @@
+package in.com.beans;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportService {
+
+	private ReportDao dao;
+
+	public ReportService(ReportDao dao) {
+		System.out.println("ReportService:: Param - Constructor");
+		this.dao = dao;
+	}
+
+	public void generateReport() {
+		dao.getData();
+		System.out.println("Report Generated.....");
+	}
+}
