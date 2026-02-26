6aee8f3ac8d2bd2dcad4e6f68886441ed4e6ce47
Yagnesh Lakshman Sai <d.yagnesh.lakshman.sai@gmail.com>
2026-02-26 22:18:20 +0530

    Create Lombok Project

---
+++ 08-Lombok-App/src/main/java/in/com/Test.java
@@ -0,0 +1,15 @@
+package in.com;
+
+import in.com.dto.Person;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setPersonId(101);
+		
+		System.out.println(p);
+		
+	}
+
+}
